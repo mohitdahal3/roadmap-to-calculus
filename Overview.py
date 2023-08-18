674d9f1885acf82ee5f9ac5cd7429f604957a251
Mohit Dahal <mohitdahal3@gmail.com>
2023-08-19 00:21:02 +0545

    Added overview.py

--- Overview.py
+++ Overview.py
@@ -1,6 +1,13 @@
 from manim import *
 from math import sin , cos
 
+
+def video_icon():
+    t = Triangle(stroke_color=WHITE , fill_color=WHITE, fill_opacity = 1).rotate(-PI/2).scale(0.5).shift(RIGHT * 0.1 + DOWN * 0.25)
+    box = RoundedRectangle(stroke_color=DARK_BLUE , fill_color=DARK_BLUE, fill_opacity = 1, corner_radius=0.4 , width=2, height=1.7)
+    return VGroup(box,t)
+
+
 def Tick(x_pos , y_pos , color):
     l1 = Line(start=[x_pos+0.15,y_pos+0.39,0] , end=[x_pos+0.3,y_pos,0] , color=color)
     l2 = Line(start=[x_pos+0.3,y_pos,0] , end=[x_pos+1,y_pos+0.7,0] , color=color)
@@ -9,94 +16,124 @@ def Tick(x_pos , y_pos , color):
 def Vec(x_pos , y_pos , l , theta):
     return Arrow(start = [x_pos , y_pos , 0] , end=[x_pos + l * cos(theta) , y_pos + l * sin(theta) ,0])
 
-class Scene1(Scene):
+
+def Flag():
+    triangle = Triangle(stroke_width = 0 , fill_color = YELLOW , fill_opacity = 1).rotate(PI/2)
+    line = Line(UP * 1.6 , DOWN * 1.6, color=YELLOW , stroke_width = 2).next_to(triangle , RIGHT ,buff=0).align_to(triangle , UP).shift(DOWN * 0.008)
+    base = Line(LEFT * 0.4 , RIGHT * 0.4, color=YELLOW , stroke_width = 2).next_to(line , DOWN , buff=0) 
+    return VGroup(base , line , triangle)
+
+
+class Scene1(ThreeDScene):
     def construct(self):
-        # self.add(NumberPlane())
-        self.wait()
-        welcome_guys = Tex("Welcome guys!",color=BLUE).scale(1.2)
-        calculus = Tex("Calculus",color=GREEN)
-        ar = Arrow(start=[-0.65,0,0] , end=[0.65,0,0])
-        text = Tex("Math").shift(RIGHT*1.65)
-        self.play(FadeIn(welcome_guys))
-        self.wait()
-        self.play(welcome_guys.animate.to_edge(UP),Write(calculus))
-        self.play(calculus.animate.shift(LEFT*1.65) , FadeIn(ar) , FadeIn(text))
-        self.play(FadeOut(text))
-        text.become(Tex("Physics").shift(RIGHT*1.65))
-        self.play(FadeIn(text))
-        self.play(FadeOut(text))
-        text.become(Tex("Economics").shift(RIGHT*1.65))
-        self.play(FadeIn(text))
-        self.play(FadeOut(text))
-        text.become(Tex("Statistics").shift(RIGHT*1.65))
-        self.play(FadeIn(text))
-        self.play(FadeOut(text))
-        text.become(Tex("Business").shift(RIGHT*1.65))
-        self.play(FadeIn(text))
-        self.play(FadeOut(text))
-        text.become(Tex("Many Things!").shift(RIGHT*1.65).scale(0.75))
-        self.play(FadeIn(text))
+        helloText = Tex("Hello" , color=YELLOW)
+        nameText = Tex(", " , "Mohit here").shift(RIGHT * 0.94)
+        nameText[0].set_color(YELLOW)
+        nameText[1].set_color(BLUE)
+        uniqueHobby = Tex("Unique hobby" , color=BLUE)
+        calculsText = Tex("Calculus" , color=YELLOW).scale(0.8).shift(UP * 3 + RIGHT * 0.7)
+        flag = Flag().scale(0.2).next_to(calculsText , LEFT).align_to(calculsText , DOWN)
+        titleText = Tex("Roadmap to Calculus" , color=BLUE).scale(1.3).rotate(65 * DEGREES , axis=X_AXIS)
+        canvas = Rectangle(color=BLACK , height=10 , width=16 , stroke_width = 0).set_opacity(0.85)
 
+
+        points = [ 
+            [-0.85 , -1.9 , 0],
+            [1.9 , 0.25 , 0],
+            [-1.78 , 1.1 , 0],
+            [0.3 , 2.91 , 0]
+        ]
+        path1 = CubicBezier(points[0] , points[1] , points[2] , points[3]).reverse_points().shift(LEFT + DOWN * 0.5)
+        path2 = CubicBezier(points[0] , points[1] , points[2] , points[3]).reverse_points().shift(RIGHT + DOWN * 0.5)
+
+        self.play(Write(helloText))
+        self.wait()
+        self.play(helloText.animate.shift(LEFT * 1) , Write(nameText))
+        self.wait(2)
+        self.play(FadeOut(helloText , nameText , run_time = 0.5) , Write(uniqueHobby))
+        self.wait()
+        self.play(Create(path1 , run_time = 1.3) , Create(path2 , run_time = 1.3) , FadeOut(uniqueHobby , run_time = 0.5))
+        self.play(AnimationGroup( Write(calculsText , rate_func=linear) , GrowFromCenter(flag , rate_func = rush_into , run_time = 0.7) , lag_ratio=0.3) )
+        self.play(Flash(flag[2].get_corner(DR), line_length=0.4, time_width=0.4 , run_time = 1 , rate_func = rush_from))
+        self.wait()
+        self.move_camera(phi=65*DEGREES)
+        self.play(AnimationGroup(FadeIn(canvas) , Write(titleText) , lag_ratio=0.5) , rate_func=linear , run_time = 2)
+        self.wait()
         
-        self.play(FadeOut(calculus,text,ar))
-        self.wait()
-
 
 class Scene2(Scene):
     def construct(self):
-        # self.add(NumberPlane())
-        welcome_guys = Tex("Welcome guys!",color=BLUE).scale(1.2).to_edge(UP)
-        self.add(welcome_guys)
-        teacher = Tex("Teacher",color=GREEN).shift(UP*0.5)
-        you = Tex("You",color=GREEN).shift(DOWN*0.5)
-        t1 = Tick(1,0,YELLOW)
-        t2 = Tick(1,-1,YELLOW)
-        t3 = Tick(3 , -0.5 , WHITE)
-        calculus = Tex("Calculus").shift(RIGHT * 2.1)
-        ar = Arrow(start=[-1,0,0] , end=[1,0,0])
-        self.play(Write(teacher))
-        self.play(FadeIn(t1))
-        self.play(Write(you))
-        self.play(FadeIn(t2))
-        
-        self.play(teacher.animate.shift(LEFT * 3) , you.animate.shift(LEFT * 3) , t1.animate.shift(LEFT * 3) , t2.animate.shift(LEFT * 3) , FadeIn(ar))
-        self.play(Write(calculus))
-        self.play(FadeIn(t3))
+        i1 = video_icon()
+        i2 = video_icon()
+        i3 = video_icon()
+        i4 = video_icon()
+        i5 = video_icon()
+        i6 = video_icon()
+        i7 = video_icon()
+        i8 = video_icon()
+        i9 = video_icon()
+
+        iconGroup = VGroup(i1 , i2 , i3 , i4 , i5 , i6 , i7 , i8 , i9).arrange(buff=1.7).scale(0.4).to_edge(UP)
+        iconGroupCopy = iconGroup.copy()
+        text1 = Tex("Functions").scale(0.9).shift(DOWN * 1.3)
+        text2 = Tex("Slope").scale(0.9).shift(DOWN * 1.3)
+        text3 = Tex("Limits").scale(0.9).shift(DOWN * 1.3)
+        text4 = Tex("Derivatives and Integrals").scale(0.9).shift(DOWN * 1.3)
+        self.play(
+            AnimationGroup(*[
+                DrawBorderThenFill(iconGroup[i]) for i in range(iconGroup.__len__())
+            ] , lag_ratio=0.5) , run_time = 1.2
+        )
+
+        self.play(i2.animate.move_to(ORIGIN).scale(2) , FadeIn(text1))
+
+        self.wait(0.5)
+        self.play(i2.animate.move_to(iconGroupCopy[1]).scale(0.5), i3.animate.move_to(ORIGIN).scale(2) , FadeTransform(text1 , text2))
+        self.wait(0.5)
+        self.play(i3.animate.move_to(iconGroupCopy[2]).scale(0.5), i5.animate.move_to(ORIGIN).scale(2) , FadeTransform(text2 , text3))
+        self.wait(0.5)
+        self.play(i5.animate.move_to(iconGroupCopy[4]).scale(0.5), VGroup(i8 , i9).animate.move_to(ORIGIN).scale(2) , FadeTransform(text3 , text4))
+        self.wait(0.5)
+        self.play(i8.animate.move_to(iconGroupCopy[7]).scale(0.5), i9.animate.move_to(iconGroupCopy[8]).scale(0.5) , FadeOut(text4))
 
         self.wait()
-
 
 class Scene3(Scene):
     def construct(self):
-        # self.add(NumberPlane())
-        img = ImageMobject("3b1b.jpg").scale(0.35).shift(LEFT * 1.85)
-        t = Tex('3Blue1Brown').next_to(img , RIGHT)
-        
-        self.play(FadeIn(img) , Write(t))
+        logo3b1b = ImageMobject('./3b1b.jpg').scale(0.3)
+        # text = Text("3Blue1Brown" , t2c={'3Blue':BLUE , '1Brown':LIGHT_BROWN})
+        text = Tex("3Blue", "1Brown" , color=LIGHT_BROWN).scale(1.3)
+        text[0].set_color(BLUE)
+        logo = Group(logo3b1b , text).arrange()
+        self.play(FadeIn(logo3b1b) , Write(text))
+        self.wait(2)
+        self.play(FadeOut(logo))
+
+        banner = ManimBanner().scale(0.4)
+        self.play(banner.create())
+        self.play(banner.expand())
 
         self.wait()
-
 
 class Scene4(Scene):
     def construct(self):
-        # self.add(NumberPlane())
-        l1 = Line(start=[-3,-2,0] , end=[-3,-1,0])
-        l2 = Line(start=[-3,-1,0] , end=[-1,-1,0])
-        l3 = Line(start=[-1,-1,0] , end=[-1,0,0])
-        l4 = Line(start=[-1,0,0] , end=[1,0,0])
-        l5 = Line(start=[1,0,0] , end=[1,1,0])
-        l6 = Line(start=[1,1,0] , end=[3,1,0])
-        f = Tex("Functions").shift(LEFT * 2 + DOWN * 0.5).scale(0.9)
-        l = Tex("Limits").shift(UP * 0.5)
-        c = Tex("Calculus").shift(RIGHT * 2 + UP * 1.5)
+        i1 = video_icon()
+        i2 = video_icon()
+        i3 = video_icon()
+        i4 = video_icon()
+        i5 = video_icon()
+        i6 = video_icon()
+        i7 = video_icon()
+        i8 = video_icon()
+        i9 = video_icon()
 
-        self.play(FadeIn(l1,l2)) 
-        self.play(Write(f))
-        self.play(FadeIn(l3,l4)) 
-        self.play(Write(l))
-        self.play(FadeIn(l5,l6)) 
-        self.play(Write(c))
+        iconGroup = VGroup(i1 , i2 , i3 , i4 , i5 , i6 , i7 , i8 , i9).arrange(buff=1.7).scale(0.4).to_edge(UP)
+        self.add(iconGroup)
 
-        a = Vec(-3,0,4.5,0.46)
-        self.play(Create(a))
+        self.play(
+            AnimationGroup(*[
+                iconGroup[i].animate(rate_func = there_and_back).shift(DOWN * 0.5) for i in range(1 , iconGroup.__len__())
+            ] , lag_ratio=0.35)
+        )
         self.wait()
+        self.play(*[FadeOut(mob)for mob in self.mobjects])
